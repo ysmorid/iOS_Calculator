3b8ef1e4702cc13c08f00b08836da054a2b14e58
Ylia M <ysmorid@users.noreply.github.com>
2017-04-05 12:39:28 -0500

    Update clear to remove descriptionDisplay and set accumulator back to 0

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -4,7 +4,7 @@ struct CalculatorBrain {
     
     private var accumulator: Double?
     private var pendingBinaryOperation: PendingBinaryOperation?
-    private var description: String = ""
+    private var description: String = " "
     private var isPartialResult: Bool = true
     
     private enum Operation {
@@ -12,6 +12,7 @@ struct CalculatorBrain {
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double, Double) -> Double)
         case equals
+        case clear
     }
     
     private var operations: Dictionary<String, Operation> = [
@@ -23,12 +24,12 @@ struct CalculatorBrain {
         "tan": Operation.unaryOperation(tan),
         "±": Operation.unaryOperation({-$0}),
         "x²": Operation.unaryOperation({$0 * $0}),
-        "C": Operation.unaryOperation({$0 * 0}),
         "+": Operation.binaryOperation({$0 + $1}),
         "-": Operation.binaryOperation({$0 - $1}),
         "x": Operation.binaryOperation({$0 * $1}),
         "/": Operation.binaryOperation({$0 / $1}),
-        "=": Operation.equals
+        "=": Operation.equals,
+        "C": Operation.clear
     ]
     
     var result: Double? {
@@ -64,7 +65,10 @@ struct CalculatorBrain {
             case .equals:
                 performPendingBinaryOperation()
                 isPartialResult = false
+            case .clear:
+                clear()
             }
+            
         }
         
     }
@@ -86,11 +90,19 @@ struct CalculatorBrain {
     }
     
     mutating func displayDescription() -> String{
+        if accumulator == 0.0{
+            return description
+        }
         if isPartialResult {
             return description + "..."
         }
         else {
             return description + "="
         }
+    }
+    
+    private mutating func clear() {
+        accumulator = 0.0
+        description = " "
     }
 }
